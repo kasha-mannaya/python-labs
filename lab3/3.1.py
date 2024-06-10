bb70efa196b688368216b60dcffe84b5e6c5ab91
kasha-mannaya <151981312+kasha-mannaya@users.noreply.github.com>
2024-06-10 16:19:50 +0200

    Add files via upload

---
+++ lab3/3.1.py
@@ -0,0 +1,18 @@
+a = input("Введите строку: ")
+answer = ""
+i = 0
+while i < len(a):
+	if i == len(a) - 1:
+		answer += a[i]
+		break
+	counter = 1
+	while (a[i] == a[counter + i]):
+		counter += 1
+		if (counter + i >= len(a) ):
+			break
+	answer += a[i]
+	if counter != 1:
+		answer += str(counter)
+		i += counter - 1
+	i += 1
+print(answer)
