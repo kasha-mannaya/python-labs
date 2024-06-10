d6e5fcd23d5318e9590d42e50d97b71fab009d6b
kasha-mannaya <151981312+kasha-mannaya@users.noreply.github.com>
2024-06-10 16:28:31 +0200

    Add files via upload

---
+++ lab3/3.4.py
@@ -0,0 +1,10 @@
+data = {}
+a = list(input("enter line: ").split())
+for elem in a:
+	if elem in data:
+		data[elem] += 1
+	else:
+		data[elem] = 1
+answer = [elem for elem in data.values()]
+for i in answer:
+	print(i, end="\t")
